728e65f603e0aefa87ca61d6be80f43eba9fad7d
Alice Lui <aytlui@gmail.com>
2021-03-09 15:36:02 +0000

    up to 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,9 +5,37 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
+
+  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it {is_expected.to respond_to(:bike)}
+
+  it 'docks something' do 
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises error when there are no bikes avialable' do
+      expect{subject.release_bike}.to raise_error 'No bikes avialable'
+    end
+
   end
 
 end
